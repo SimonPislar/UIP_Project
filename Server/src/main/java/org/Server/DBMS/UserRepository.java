eb9481f8a7076769d2f2e05c8905ad2417c299f8
SimonPislar <51m0np15l3r@gmail.com>
2024-04-09 03:03:42 +0200

    Implement an interface for easier communication with the user info in the DB

---
+++ Server/src/main/java/org/Server/DBMS/UserRepository.java
@@ -0,0 +1,12 @@
+package org.Server.DBMS;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    public Optional<User> findByEmail(String email);
+}
