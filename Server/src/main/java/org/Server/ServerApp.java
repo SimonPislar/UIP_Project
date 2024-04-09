d2840cbd6f7775e9d01e72d59709b7b66687cf6b
SimonPislar <51m0np15l3r@gmail.com>
2024-04-09 03:02:21 +0200

    Implement Main method

---
+++ Server/src/main/java/org/Server/ServerApp.java
@@ -0,0 +1,9 @@
+package org.Server;
+
+import org.springframework.boot.SpringApplication;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApp.class, args);
+    }
+}
