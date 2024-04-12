6f69f133132b57a730c083cf030e2dea5009a4e7
SimonPislar <51m0np15l3r@gmail.com>
2024-04-12 11:42:44 +0200

    Add RequestMapping notation

--- Server/src/main/java/org/Server/Communications/Receiver.java
+++ Server/src/main/java/org/Server/Communications/Receiver.java
@@ -1,34 +1,29 @@
 package org.Server.Communications;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path="/receiver") // This means URLs start with /user-controller (after Application path)
 public class Receiver {
 
-    @Autowired
-    public Receiver() {
-
-    }
-
     /*
-        @Brief: This function is used to login a user
+        @Brief: This function is used to log in a user
         @Param: email - The email of the user to be logged in.
         @Param: password - The password of the user to be logged in.
         @Return: String - Returns "true" if the user is logged in.
      */
-    @GetMapping("/receiver/login")
+    @GetMapping(path = "login")
     public String login(@RequestParam (value = "email") String email,
                         @RequestParam (value = "password") String password) {
         return "true";
     }
 
     /*
-        @Brief: This function is used to logout a user
+        @Brief: This function is used to log out a user
         @Param: email - The email of the user to be logged out.
         @Return: void - Returns nothing.
      */
-    @PostMapping("/receiver/logout")
+    @PostMapping(path = "logout")
     public void logout(@RequestParam String email) {
 
     }
@@ -40,7 +35,7 @@ public class Receiver {
         @Param: password - The password of the user to be registered.
         @Return: void - Returns nothing.
      */
-    @PostMapping("/receiver/register-account")
+    @PostMapping(path = "register-account")
     public void accountRegister(@RequestParam String email,
                                 @RequestParam String username,
                                 @RequestParam String password) {
@@ -53,7 +48,7 @@ public class Receiver {
         @Param: password - The password of the user to be deleted.
         @Return: String - Returns "deleted" if the user is deleted.
      */
-    @DeleteMapping("/receiver/delete-account")
+    @DeleteMapping(path = "delete-account")
     public String deleteAccount(@RequestParam String email,
                                 @RequestParam String password) {
         return "";
@@ -66,7 +61,7 @@ public class Receiver {
         @Param: password - The password of the user to be updated.
         @Return: String - Returns "updated" if the user is updated.
      */
-    @PostMapping("/receiver/update-account")
+    @PostMapping(path = "update-account")
     public String updateAccount(@RequestParam String email,
                                 @RequestParam String username,
                                 @RequestParam String password) {
@@ -78,7 +73,7 @@ public class Receiver {
         @Param: email - The email of the user to be retrieved.
         @Return: String - Returns the user account.
      */
-    @GetMapping("/receiver/get-account")
+    @GetMapping(path = "get-account")
     public String getAccount(@RequestParam String email) {
         return "";
     }
@@ -89,7 +84,7 @@ public class Receiver {
         @Param: lobbyName - The name of the lobby to be created.
         @Return: void - Returns nothing.
      */
-    @PostMapping("/receiver/create-lobby")
+    @PostMapping(path = "create-lobby")
     public void createLobby(@RequestParam String email,
                             @RequestParam String lobbyName) {
 
@@ -101,7 +96,7 @@ public class Receiver {
         @Param: lobbyName - The name of the lobby to be joined.
         @Return: String - Returns nothing.
      */
-    @PostMapping("/receiver/join-lobby")
+    @PostMapping(path = "join-lobby")
     public String joinLobby(@RequestParam String email,
                           @RequestParam String lobbyName) {
         return "";
@@ -113,7 +108,7 @@ public class Receiver {
         @Param: lobbyName - The name of the lobby to be left.
         @Return: void - Returns nothing.
      */
-    @PostMapping("/receiver/leave-lobby")
+    @PostMapping(path = "leave-lobby")
     public void leaveLobby(@RequestParam String email,
                            @RequestParam String lobbyName) {
 
@@ -125,7 +120,7 @@ public class Receiver {
         @Param: lobbyName - The name of the lobby to start the game.
         @Return: void - Returns nothing.
      */
-    @PostMapping("/receiver/start-game")
+    @PostMapping(path = "start-game")
     public void startGame(@RequestParam String email,
                           @RequestParam String lobbyName) {
 
@@ -137,7 +132,7 @@ public class Receiver {
         @Param: lobbyName - The name of the lobby to end the game.
         @Return: void - Returns nothing.
      */
-    @PostMapping("/receiver/end-game")
+    @PostMapping(path = "end-game")
     public void endGame(@RequestParam String email,
                         @RequestParam String lobbyName) {
 
@@ -147,7 +142,7 @@ public class Receiver {
         @Brief: This function is used to check server status
         @Return: String - Returns "pong".
     */
-    @GetMapping("/receiver/ping")
+    @GetMapping(path = "ping")
     public String ping() {
         return "pong";
     }
