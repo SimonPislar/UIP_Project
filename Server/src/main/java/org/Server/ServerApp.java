79da2a29424e041aa87909822189984ba364348c
SimonPislar <51m0np15l3r@gmail.com>
2024-04-12 11:43:54 +0200

    Add SpringBootApplication-notation

--- Server/src/main/java/org/Server/ServerApp.java
+++ Server/src/main/java/org/Server/ServerApp.java
@@ -1,8 +1,11 @@
 package org.Server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ServerApp {
+
     public static void main(String[] args) {
         SpringApplication.run(ServerApp.class, args);
     }
