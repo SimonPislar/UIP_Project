2870a38efe0584d6a308f831e6a2ef8a074eaff5
SimonPislar <51m0np15l3r@gmail.com>
2024-04-19 15:08:45 +0200

    Remove methods that have been phased out and add template for new function

--- Server/src/main/java/org/Server/Communications/Sender.java
+++ Server/src/main/java/org/Server/Communications/Sender.java
@@ -14,43 +14,19 @@ public class Sender {
     }
 
     /*
-        @Brief: This function notifies the client that the guess round has started
-        @Return: void - Returns nothing.
-     */
-    public void guessRoundStart() {
-
-    }
-
-    /*
-        @Brief: This function notifies the client that the guess round has ended
-        @Return: void - Returns nothing.
-     */
-    public void guessRoundEnd() {
-
-    }
-
-    /*
-        @Brief: This function notifies the client that the drawing round has started
-        @Return: void - Returns nothing.
-     */
-    public void drawRoundStart() {
-
-    }
-
-    /*
-        @Brief: This function notifies the client that the drawing round has ended
-        @Return: void - Returns nothing.
-     */
-    public void drawRoundEnd() {
-
-    }
-
-    /*
         @Brief: This function notifies the client that the game has unexpectedly ended
         @Param: message - The message to be sent to the client.
         @Return: void - Returns nothing.
     */
     public void cancelGame(String message) {
+
+    }
+
+    /*
+        @Brief: This function notifies the client that the game has ended
+        @Return: void - Returns nothing.
+    */
+    public void notifyGameStart() {
 
     }
 
