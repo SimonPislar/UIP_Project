d90145b5d014fba0d96a667ee618f6a02d208ae7
SimonPislar <51m0np15l3r@gmail.com>
2024-05-16 01:07:40 +0200

    Implement react router for navigating between components

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './CSS/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import SignInPage from "./SignInPage";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/sign-in" element={<SignInPage/>} />
+        </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
