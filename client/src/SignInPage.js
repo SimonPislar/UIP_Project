656278d1c9bd41a3923b78d2d8f5c9c89ee04fbf
SimonPislar <51m0np15l3r@gmail.com>
2024-05-17 00:15:22 +0200

    Add connections to backend

--- client/src/SignInPage.js
+++ client/src/SignInPage.js
@@ -2,11 +2,15 @@ import React, {useState} from "react";
 import Input from "./Input";
 import './CSS/SignInPage.css';
 import Button from "./Button";
+import {useNavigate} from "react-router-dom";
 
 function SignInPage() {
 
+    let displayLoginError = false;
+
     const [username, setUsername] = useState(''); // username is the state variable, setUsername is the function that updates the state variable
     const [password, setPassword] = useState(''); // password is the state variable, setPassword is the function that updates the state variable
+    const navigate = useNavigate();
 
     const handleUsernameChange = (event) => {
         setUsername(event.target.value);
@@ -18,10 +22,31 @@ function SignInPage() {
 
     const handleSignIn = () => {
         console.log("Sign In");
+        const formData = new URLSearchParams();
+        formData.append('username', username);
+        formData.append('password', password);
+        fetch('http://localhost:8080/receiver/login', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then((response) => response.text())
+            .then((data) => {
+                if (data === "true") {
+                    displayLoginError = false;
+                    navigate('/home');
+                } else {
+                    console.log("Incorrect username or password");
+                    displayLoginError = true;
+                }
+            }
+        );
     }
 
     const handleSignUp = () => {
-        console.log("Sign Up");
+        console.log("Register account");
+        navigate('/register');
     }
 
     return (
@@ -38,6 +63,13 @@ function SignInPage() {
             <div className="input-container">
                 <Button size="small" text="Sign In" onClick={handleSignIn} />
             </div>
+
+            {displayLoginError && (
+                <div className="input-container error-message">
+                    <p>Incorrect username or password</p>
+                </div>
+            )}
+
             <div className="input-container help">
                 <div className="sign-up-help">
                     <p>Forgot your password?</p>
