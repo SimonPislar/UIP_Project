d8d3cdc84ee15fdef1acefc09aee286a59ab737b
SimonPislar <51m0np15l3r@gmail.com>
2024-05-17 00:16:41 +0200

    Implement encryption methods

--- Server/src/main/java/org/Server/Security/SecurityTools.java
+++ Server/src/main/java/org/Server/Security/SecurityTools.java
@@ -2,17 +2,23 @@ package org.Server.Security;
 
 import org.springframework.stereotype.Component;
 import java.util.UUID;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @Component
 public class SecurityTools {
 
-    public SecurityTools() {
+    Argon2PasswordEncoder argon2PasswordEncoder;
 
+    public SecurityTools() {
+        argon2PasswordEncoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8(); // Encode with the default values
     }
 
-    public UUID generateID() {
-        UUID id = UUID.randomUUID();
-        return id;
+    public String encrypt(String password) {
+        return this.argon2PasswordEncoder.encode(password);
+    }
+
+    public Boolean comparePasswords(String password, String encodedPassword) {
+        return this.argon2PasswordEncoder.matches(password, encodedPassword);
     }
 
 }
