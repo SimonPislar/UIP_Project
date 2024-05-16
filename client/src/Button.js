d98444125447ece75133db1dd87b74fee84eef2d
SimonPislar <51m0np15l3r@gmail.com>
2024-05-16 18:48:36 +0200

    Add onClick variable for button component

--- client/src/Button.js
+++ client/src/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './CSS/Button.css';
 
-function Button({ size, text }) {
+function Button({ size, text, onClick}) {
     return (
-        <button className={`button ${size}`}>{text}</button>
+        <button className={`button ${size}`} onClick={onClick}>{text}</button>
     );
 }
 
