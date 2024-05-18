51f41e4358b284918948f332f5f5011aa7625c2e
SimonPislar <51m0np15l3r@gmail.com>
2024-05-18 16:58:51 +0200

    Add shell code to account component

---
+++ client/src/Account.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {useLocation} from "react-router-dom";
+
+function Account() {
+
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const email = queryParams.get('email');
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Account;
