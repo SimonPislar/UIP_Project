7d197743f009ebf357e2bb5fd89f0a80fc381d7c
SimonPislar <51m0np15l3r@gmail.com>
2024-05-21 02:05:57 +0200

    Implement most of Lobby-component

--- client/src/Lobby.js
+++ client/src/Lobby.js
@@ -1,5 +1,7 @@
-import React, {useEffect, useState} from "react";
-import {useLocation} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import './CSS/Lobby.css';
+import Button from "./Button";
 
 function Lobby() {
 
@@ -8,6 +10,8 @@ function Lobby() {
     const email = queryParams.get('email');
     const [lobbyName, setLobbyName] = useState('');
     const [lobbyFetched, setLobbyFetched] = useState(false);
+    const [players, setPlayers] = useState([]);
+    const [isHost, setIsHost] = useState(false);
 
     useEffect(() => {
         console.log("Lobby mounted")
@@ -21,21 +25,58 @@ function Lobby() {
             }
         }).then((response) => response.json())
             .then((data) => {
-                    if (data.success) {
-                        console.log(data);
-                        // eslint-disable-next-line react-hooks/exhaustive-deps
-                        setLobbyFetched(true);
-                        setLobbyName(data.message)
-                    } else {
-                        console.log(data.message);
+                if (data.success) {
+                    console.log(data);
+                    setLobbyFetched(true);
+                    setLobbyName(data.message)
+                    if (data.isHost) {
+                        setIsHost(true);
                     }
+                    const formData = new URLSearchParams();
+                    formData.append('gameName', data.message);
+                    fetch('http://192.168.0.17:8080/receiver/get-lobby-players', {
+                        method: 'POST',
+                        body: formData,
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    }).then((response) => response.json())
+                        .then((data) => {
+                                if (data.success) {
+                                    console.log(data.message);
+                                    setPlayers(data.message)
+                                } else {
+                                    console.log(data.message);
+                                }
+                            }
+                        );
+                } else {
+                    console.log(data.message);
                 }
-            );
+            }
+        );
     }, []);
 
     return (
-        <div>
-            {lobbyFetched && <h1>{lobbyName}</h1>}
+        <div className="lobby-container">
+            <div>
+                {lobbyFetched && <h1>{lobbyName}</h1>}
+            </div>
+            <div className="players-list-container">
+                <div className="players-list">
+                    {players.map((player, index) => (
+                        <div key={index} className="player-item">
+                            {player}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="buttons-container">
+                <Button size="small" text="Leave" onClick={() => console.log("Leave")} /> {/* Implement Leave button */}
+                {isHost &&
+                    <Button size="small" text="Start" onClick={() => console.log("Start")} /> /* Implement Start button */
+                }
+            </div>
         </div>
     );
 }
