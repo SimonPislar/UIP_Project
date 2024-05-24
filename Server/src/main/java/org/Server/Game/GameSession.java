2e47ff39413c303560e30b092d27395b53df44da
SimonPislar <51m0np15l3r@gmail.com>
2024-05-25 01:52:32 +0200

    Implement support for handling SketchBooks and words

--- Server/src/main/java/org/Server/Game/GameSession.java
+++ Server/src/main/java/org/Server/Game/GameSession.java
@@ -1,28 +1,68 @@
 package org.Server.Game;
 
+import org.Server.Pair;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class GameSession {
 
     private int playerCount;
     private final ArrayList<Player> players;
-    private String[] words;
-    private final ArrayList<Round> rounds;
+    private final ArrayList<Word> originalWords;
     private final String sessionName;
+    private final ArrayList<Player> gameOrder;
 
     /*
         @Brief: This constructor is used to initialize the game session.
         @Param: playerCount - The player count for the game session.
         @Param: playerName - The name of the player.
     */
-
     public GameSession(int playerCount, Player player, String sessionName) {
         this.playerCount = playerCount;
-        this.words = new String[playerCount];
+        this.originalWords = new ArrayList<>();
         this.players = new ArrayList<>();
-        this.rounds = new ArrayList<>();
         this.sessionName = sessionName;
         this.players.add(player);
+        this.gameOrder = new ArrayList<>();
+    }
+
+    /*
+        @Brief: This function is used to rotate the sketch books of the players.
+        @Return: void - Returns nothing.
+     */
+    public void rotateSketchBooks() {
+        ArrayList<SketchBook> sketchBooks = new ArrayList<>();
+        this.gameOrder.forEach(player -> {
+            sketchBooks.add(player.getSketchBook());
+        });
+
+        for (int i = 0; i < this.gameOrder.size(); i++) {
+            this.gameOrder.get(i).setSketchBook(sketchBooks.get((i + 1) % gameOrder.size()));
+        }
+    }
+
+
+    public void setGameOrder() {
+        this.gameOrder.addAll(this.players);
+        Collections.shuffle(this.gameOrder);
+    }
+
+    /*
+        @Brief: This function is used to add a word to the game session along with its author.
+        @Param: word - The word and author to be added.
+        @Return: void - Returns nothing.
+    */
+    public void addOriginalWord(Word word) {
+        this.originalWords.add(word);
+    }
+
+    public ArrayList<Word> getOriginalWords() {
+        return this.originalWords;
+    }
+
+    public ArrayList<Player> getGameOrder() {
+        return this.gameOrder;
     }
 
     /*
@@ -33,13 +73,6 @@ public class GameSession {
         return this.sessionName;
     }
 
-    /*
-        @Brief: This function is used to get the rounds for the game session.
-        @Return: ArrayList<Round> - Returns the rounds.
-    */
-    public ArrayList<Round> getRounds() {
-        return this.rounds;
-    }
 
     /*
         @Brief: This function is used to get the round count for the game session.
@@ -63,14 +96,6 @@ public class GameSession {
     }
 
     /*
-        @Brief: This function is used to get the starting words for the game session.
-        @Return: String[] - Returns the words.
-    */
-    public String[] getWords() {
-        return this.words;
-    }
-
-    /*
         @Brief: This function is used to get the player count for the game session.
         @Return: int - Returns the player count.
     */
@@ -88,36 +113,12 @@ public class GameSession {
     }
 
     /*
-        @Brief: This function is used to set the words for the game session.
-        @Param: words - The words to be set.
-        @Return: void - Returns nothing.
-    */
-    public void setWords(String[] words) {
-        if (words.length > this.playerCount) {
-            String[] shortenedWords = new String[this.playerCount];
-            System.arraycopy(words, 0, shortenedWords, 0, this.playerCount);
-            this.words = shortenedWords;
-        } else {
-            this.words = words;
-        }
-    }
-
-    /*
         @Brief: This function is used to append a player to the game session.
         @Param: player - The player to be appended.
         @Return: void - Returns nothing.
     */
     public void appendPlayer(Player player) {
         this.players.add(player);
-    }
-
-    /*
-        @Brief: This function is used to append a round to the game session.
-        @Param: round - The round to be appended.
-        @Return: void - Returns nothing.
-    */
-    public void appendRound(Round round) {
-        this.rounds.add(round);
     }
 
     /*
@@ -134,8 +135,7 @@ public class GameSession {
         @Return: void - Returns nothing.
     */
     public void reset() {
-        this.rounds.clear();
-        this.words = new String[this.playerCount];
         this.players.forEach(Player::resetPoints);
+        this.originalWords.clear();
     }
 }
