3d5f6286a6370330a491e3cd512b00faff185ac0
SimonPislar <51m0np15l3r@gmail.com>
2024-05-24 02:24:01 +0200

    Implement sender-class for sending messages via websocket

--- Server/src/main/java/org/Server/Communications/Sender.java
+++ Server/src/main/java/org/Server/Communications/Sender.java
@@ -1,35 +1,29 @@
 package org.Server.Communications;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.TextMessage;
 
-@Component
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/sender")
 public class Sender {
 
-    /*
-        @Brief: This function notifies the client that drawing has ended
-        @Return: void - Returns nothing.
-     */
-    public void drawTimeIsUp() {
+    WebSocketHandler webSocketHandler;
 
+    @Autowired
+    public Sender(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
     }
 
-    /*
-        @Brief: This function notifies the client that the game has unexpectedly ended
-        @Param: message - The message to be sent to the client.
-        @Return: void - Returns nothing.
-    */
-    public void cancelGame(String message) {
-
+    @PostMapping("/start-game")
+    public void startGame(@RequestParam String email) throws Exception {
+        webSocketHandler.handleTextMessage(null, new TextMessage("{\"message\":\"Start\", \"email\":\"" + email + "\"}"));
     }
-
-    /*
-        @Brief: This function notifies the client that the game has ended
-        @Return: void - Returns nothing.
-    */
-    public void notifyGameStart() {
-
-    }
-
-
 
 }
