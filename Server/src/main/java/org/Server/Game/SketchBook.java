cbe0f513d48f4d4bf20d79ad3b2cc43dcf2d5ee7
SimonPislar <51m0np15l3r@gmail.com>
2024-05-25 01:56:48 +0200

    Implement SketchBook class for simulating game

---
+++ Server/src/main/java/org/Server/Game/SketchBook.java
@@ -0,0 +1,39 @@
+package org.Server.Game;
+
+import java.util.ArrayList;
+
+public class SketchBook {
+
+    private final String ownersEmail;
+
+    private final String originalWord;
+
+    private final ArrayList<Drawing> drawings;
+
+    public SketchBook(String owner, String originalWord) {
+        this.ownersEmail = owner;
+        this.originalWord = originalWord;
+        this.drawings = new ArrayList<>();
+    }
+
+    public void addDrawing(Drawing drawing) {
+        this.drawings.add(drawing);
+    }
+
+    public ArrayList<Drawing> getDrawings() {
+        return this.drawings;
+    }
+
+    public String getOwnersEmail() {
+        return this.ownersEmail;
+    }
+
+    public String getOriginalWord() {
+        return this.originalWord;
+    }
+
+    public Drawing getLastDrawing() {
+        return this.drawings.get(this.drawings.size() - 1);
+    }
+
+}
