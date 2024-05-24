aedce23bd81b9d7ec81d50f01f0e0395a0b84cef
SimonPislar <51m0np15l3r@gmail.com>
2024-05-25 01:53:56 +0200

    Revise the lobby fetch call and optimize use of websocket

--- client/src/Lobby.js
+++ client/src/Lobby.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
-import {useLocation, useNavigate} from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import './CSS/Lobby.css';
 import Button from "./Button";
 import useWebSocket from "react-use-websocket";
 
 function Lobby() {
-
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const email = queryParams.get('email');
@@ -14,19 +13,21 @@ function Lobby() {
     const [players, setPlayers] = useState([]);
     const [isHost, setIsHost] = useState(false);
 
+    const IP = 'http://192.168.0.17:8080'
+
     const WS_URL = 'ws://192.168.0.17:8080/ws';
 
-    const {sendJsonMessage, lastJsonMessage} = useWebSocket(WS_URL, {
+    const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
         queryParams: { email: email }
     });
 
     const navigate = useNavigate();
 
     useEffect(() => {
-        console.log("Lobby mounted")
+        console.log("Lobby mounted");
         const formData = new URLSearchParams();
         formData.append('email', email);
-        fetch('http://192.168.0.17:8080/receiver/get-player-lobby', {
+        fetch(IP + '/receiver/get-player-lobby', {
             method: 'POST',
             body: formData,
             headers: {
@@ -34,38 +35,37 @@ function Lobby() {
             }
         }).then((response) => response.json())
             .then((data) => {
-                if (data.success) {
-                    console.log(data);
-                    setLobbyFetched(true);
-                    setLobbyName(data.message)
-                    if (data.isHost) {
-                        setIsHost(true);
-                    }
-                    const formData = new URLSearchParams();
-                    formData.append('gameName', data.message);
-                    fetch('http://192.168.0.17:8080/receiver/get-lobby-players', {
-                        method: 'POST',
-                        body: formData,
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded'
+                    if (data.success) {
+                        console.log(data);
+                        setLobbyFetched(true);
+                        setLobbyName(data.message);
+                        if (data.isHost) {
+                            setIsHost(true);
                         }
-                    }).then((response) => response.json())
-                        .then((data) => {
-                                if (data.success) {
-                                    console.log(data.message);
-                                    setPlayers(data.message)
-                                } else {
-                                    console.log(data.message);
-                                }
+                        const formData = new URLSearchParams();
+                        formData.append('gameName', data.message);
+                        fetch(IP + '/receiver/get-lobby-players', {
+                            method: 'POST',
+                            body: formData,
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded'
                             }
-                        );
-                } else {
-                    console.log(data.message);
+                        }).then((response) => response.json())
+                            .then((data) => {
+                                    if (data.success) {
+                                        console.log(data.message);
+                                        setPlayers(data.message);
+                                    } else {
+                                        console.log(data.message);
+                                    }
+                                }
+                            );
+                    } else {
+                        console.log(data.message);
+                    }
                 }
-            }
-        );
-
-    }, []);
+            );
+    }, [email]);
 
     const handleStart = () => {
         console.log("Start button clicked");
@@ -73,15 +73,17 @@ function Lobby() {
             message: 'Start',
             email: email
         });
-    }
+    };
 
-    if (lastJsonMessage) {
-        console.log(lastJsonMessage);
-        if (lastJsonMessage.message === 'Start') {
-            console.log("Received Start message");
-            navigate(`/word-input?email=${encodeURIComponent(email)}`);
+    useEffect(() => {
+        if (lastJsonMessage) {
+            console.log(lastJsonMessage);
+            if (lastJsonMessage.message === 'Start') {
+                console.log("Received Start message");
+                navigate(`/input-word?email=${encodeURIComponent(email)}`);
+            }
         }
-    }
+    }, [lastJsonMessage, navigate, email]);
 
     return (
         <div className="lobby-container">
