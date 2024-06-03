d3ae3e7c232b070fd20eb89e305788ae4f6dd2dc
SimonPislar <51m0np15l3r@gmail.com>
2024-06-03 20:54:16 +0200

    Add shell code for end game component

---
+++ client/src/DisplayGameResult.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DisplayGameResult() {
+    return (
+        <div>
+            <h1>Game Result</h1>
+            <p>Game result will be displayed here</p>
+        </div>
+    );
+}
+
+export default DisplayGameResult;
