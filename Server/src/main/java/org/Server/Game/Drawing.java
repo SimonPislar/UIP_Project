ad0512f87ff2dba030acb44e48c02f1401dbe78b
SimonPislar <51m0np15l3r@gmail.com>
2024-06-03 20:54:34 +0200

    Implement drawing

--- Server/src/main/java/org/Server/Game/Drawing.java
+++ Server/src/main/java/org/Server/Game/Drawing.java
@@ -4,11 +4,38 @@ public class Drawing {
 
     private final String painterEmail;
 
-    public Drawing(String painterEmail) {
+    private final String drawing;
+
+    private String guesserEmail;
+
+    private String guess;
+
+    public Drawing(String painterEmail, String drawing) {
         this.painterEmail = painterEmail;
+        this.drawing = drawing;
     }
 
     public String getPainterEmail() {
         return this.painterEmail;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
+
+    public String getGuess() {
+        return this.guess;
+    }
+
+    public String getDrawingData() {
+        return this.drawing;
+    }
+
+    public void setGuesserEmail(String guesserEmail) {
+        this.guesserEmail = guesserEmail;
+    }
+
+    public String getGuesserEmail() {
+        return this.guesserEmail;
     }
 }
