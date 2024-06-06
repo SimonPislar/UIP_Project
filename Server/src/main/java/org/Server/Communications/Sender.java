e5c82b4dc183a2e6afcc0d85d59f0edaca6628a8
SimonPislar <51m0np15l3r@gmail.com>
2024-06-06 22:13:56 +0200

    Add method for sending all sketchbooks to a user

--- Server/src/main/java/org/Server/Communications/Sender.java
+++ Server/src/main/java/org/Server/Communications/Sender.java
@@ -1,7 +1,10 @@
 package org.Server.Communications;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.Server.Game.JSON.SketchbookDTO;
+import org.Server.Game.JSON.SketchbookMapper;
+import org.Server.Game.SketchBook;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,16 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.TextMessage;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
+import java.util.List;
 
 @RestController
 @RequestMapping("/sender")
 public class Sender {
 
     WebSocketHandler webSocketHandler;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public Sender(WebSocketHandler webSocketHandler) {
+    public Sender(WebSocketHandler webSocketHandler, ObjectMapper objectMapper) {
         this.webSocketHandler = webSocketHandler;
+        this.objectMapper = objectMapper;
     }
 
     @PostMapping("/start-game")
@@ -30,5 +37,15 @@ public class Sender {
         TextMessage message = new TextMessage(messageContent);
         webSocketHandler.sendMessageToUser(email, message);
         System.out.println("Message sent to " + email + ": " + messageContent);
+    }
+
+    public void sendSketchbookData(String email, List<SketchBook> sketchBooks) throws IOException {
+        List<SketchbookDTO> sketchbookDTOs = sketchBooks.stream()
+                .map(SketchbookMapper::toDTO)
+                .collect(Collectors.toList());
+        String messageContent = objectMapper.writeValueAsString(sketchbookDTOs);
+        TextMessage message = new TextMessage(messageContent);
+        webSocketHandler.sendMessageToUser(email, message);
+        System.out.println("Sketchbook data sent to " + email + ": " + messageContent);
     }
 }
