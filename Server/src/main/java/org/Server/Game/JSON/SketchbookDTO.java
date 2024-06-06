b7952b3db68aa9406bd693c195abb4352b8ce782
SimonPislar <51m0np15l3r@gmail.com>
2024-06-06 22:14:41 +0200

    Class for sending sketchbooks over json

---
+++ Server/src/main/java/org/Server/Game/JSON/SketchbookDTO.java
@@ -0,0 +1,35 @@
+package org.Server.Game.JSON;
+
+import java.util.List;
+
+public class SketchbookDTO {
+    private String ownersEmail;
+    private String originalWord;
+    private List<DrawingDTO> drawings;
+
+    // Getters and Setters
+
+    public String getOwnersEmail() {
+        return ownersEmail;
+    }
+
+    public void setOwnersEmail(String ownersEmail) {
+        this.ownersEmail = ownersEmail;
+    }
+
+    public String getOriginalWord() {
+        return originalWord;
+    }
+
+    public void setOriginalWord(String originalWord) {
+        this.originalWord = originalWord;
+    }
+
+    public List<DrawingDTO> getDrawings() {
+        return drawings;
+    }
+
+    public void setDrawings(List<DrawingDTO> drawings) {
+        this.drawings = drawings;
+    }
+}
