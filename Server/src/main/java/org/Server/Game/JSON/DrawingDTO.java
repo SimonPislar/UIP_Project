ccb4586a70820f1cd9837104d0c98ae9fd3f3085
SimonPislar <51m0np15l3r@gmail.com>
2024-06-06 22:10:19 +0200

    Component for sending drawings over JSON

---
+++ Server/src/main/java/org/Server/Game/JSON/DrawingDTO.java
@@ -0,0 +1,42 @@
+package org.Server.Game.JSON;
+
+public class DrawingDTO {
+    private String painterEmail;
+    private String drawing;
+    private String guesserEmail;
+    private String guess;
+
+    // Getters and Setters
+
+    public String getPainterEmail() {
+        return painterEmail;
+    }
+
+    public void setPainterEmail(String painterEmail) {
+        this.painterEmail = painterEmail;
+    }
+
+    public String getDrawing() {
+        return drawing;
+    }
+
+    public void setDrawing(String drawing) {
+        this.drawing = drawing;
+    }
+
+    public String getGuesserEmail() {
+        return guesserEmail;
+    }
+
+    public void setGuesserEmail(String guesserEmail) {
+        this.guesserEmail = guesserEmail;
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
+}
