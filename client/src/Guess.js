d8770afaf6a64c676687a6deecd0aead9ff90299
SimonPislar <51m0np15l3r@gmail.com>
2024-06-07 08:42:10 +0200

    Add images and tutorial mode

--- client/src/Guess.js
+++ client/src/Guess.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {useLocation, useNavigate} from "react-router-dom";
 import './CSS/Guess.css';
 import Input from "./Input";
@@ -14,12 +14,27 @@ function Guess() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const email = queryParams.get('email');
+    const tutorialString = queryParams.get('tutorial');
+    const tutorial = tutorialString === 'true';
+
+    const [isFocused, setIsFocused] = useState(false);
+    const inputRef = useRef(null);
+    const typingIntervalRef = useRef(null);
 
     const [guess, setGuess] = useState('');
 
     const handleGuessChange = (event) => {
         setGuess(event.target.value)
     }
+
+    const handleInputFocus = () => {
+        setIsFocused(true);
+        setGuess('');
+    };
+
+    const handleInputBlur = () => {
+        setIsFocused(false);
+    };
 
     const handleGuess = () => {
         console.log('Submit guess: ' + guess);
@@ -36,6 +51,10 @@ function Guess() {
             .then((data) => {
                 if (data.success) {
                     console.log('Successfully submitted guess');
+                    if (tutorial) {
+                        navigate(`/waiting-for-server?email=${encodeURIComponent(email)}&tutorial=true`);
+                        return;
+                    }
                     navigate(`/waiting-for-server?email=${encodeURIComponent(email)}`);
                 } else {
                     console.log('Failed to submit guess');
@@ -44,20 +63,69 @@ function Guess() {
         );
     }
 
+    useEffect(() => {
+        const startTyping = () => {
+            if (tutorial && guess === '') {
+                let charIndex = 0;
+                typingIntervalRef.current = setInterval(() => {
+                    setGuess((prev) => {
+                        if (charIndex === 13) {
+                            charIndex = 0;
+                            return "Your guess...".slice(0, charIndex);
+                        }
+                        charIndex++;
+                        return "Your guess...".slice(0, charIndex);
+                    });
+                }, 100);
+            }
+        };
+
+        if (!isFocused && tutorial) {
+            const delayBeforeTyping = setTimeout(startTyping, 500);
+            return () => {
+                clearTimeout(delayBeforeTyping);
+                clearInterval(typingIntervalRef.current);
+            };
+        } else {
+            clearInterval(typingIntervalRef.current);
+        }
+    }, [isFocused, tutorial]);
+
     return (
         <div className="guess-container">
-            <div className="guess-item">
-                <h1>Guess the Drawing</h1>
+            <div className="painter-container">
+                <img className="painter-img" src="/img/painter-girl.png" alt="painter"></img>
             </div>
-            <div className="guess-item">
-                <img className="drawing" src={rawDrawingData.drawing} alt="Drawing to guess" />
+            <div className="guess">
+                <div className="guess-item">
+                    <h1>Guess the Drawing</h1>
+                </div>
+                <div className="guess-item">
+                    <img className="drawing" src={rawDrawingData.drawing} alt="Drawing to guess" />
+                </div>
+                <div className="guess-item">
+                    {tutorial && (
+                        <div>
+                            <Input type="text" ref={inputRef} onFocus={handleInputFocus} onBlur={handleInputBlur} value={guess} onChange={handleGuessChange} placeholder="Guess" />
+                        </div>
+                    )}
+                    {!tutorial && (
+                        <div>
+                            <Input type="text" value={guess} onChange={handleGuessChange} placeholder="Guess" />
+                        </div>
+                    )}
+                </div>
+                <div className="guess-item">
+                    {tutorial && (
+                        <button className="begin-button-medium show" onClick={handleGuess}>Submit</button>
+                    )}
+                    {!tutorial && (
+                        <Button size="medium" text="Submit" onClick={handleGuess} />
+                    )}
+                </div>
             </div>
-            <div className="guess-item">
-                <Input size="large" value={guess} onChange={handleGuessChange} placeholder="Enter your guess here" />
-            </div>
-            <div className="guess-item">
-                <Button size="medium" onClick={handleGuess} text="Submit" />
-
+            <div className="painter-container">
+                <img className="painter-img" src="/img/painter-boy.png" alt="painter"></img>
             </div>
         </div>
     );
