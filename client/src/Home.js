c92ca927ee2ec225acbdc8b64d9feaa95044a03f
SimonPislar <51m0np15l3r@gmail.com>
2024-06-07 08:42:29 +0200

    Add images and tutorial support

--- client/src/Home.js
+++ client/src/Home.js
@@ -9,12 +9,18 @@ function Home() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const email = queryParams.get('email');
+    const tutorialString = queryParams.get('tutorial');
+    const tutorial = tutorialString === 'true';
 
     const navigate = useNavigate();
 
     const handleNewGame = () => {
         console.log("New game created")
-        navigate(`/new-game?email=${encodeURIComponent(email)}`)
+        if (tutorial) {
+            navigate(`/new-game?email=${encodeURIComponent(email)}&tutorial=true`)
+        } else {
+            navigate(`/new-game?email=${encodeURIComponent(email)}`)
+        }
     }
 
     const handleJoinGame = () => {
@@ -34,29 +40,47 @@ function Home() {
 
     const handleTutorial = () => {
         console.log("Tutorial")
+        navigate(`/tutorial?email=${encodeURIComponent(email)}`)
     }
 
     return (
         <div className="home-container">
-            <div className="img-container">
-                <img src="/img/nopfp.png" alt="pfp" width="70" height="70" onClick={handleShowAccount}></img>
-            </div>
-            <div className="tutorial-container">
-                <LinkButton size="medium" text="Don't know how to play?" onClick={handleTutorial}/>
+            {!tutorial && (
+                <div className="img-container">
+                    <img src="/img/nopfp.png" alt="pfp" width="70" height="70" onClick={handleShowAccount}></img>
+                </div>
+            )}
+            {!tutorial && (
+                <div className="tutorial-container">
+                    <LinkButton size="medium" text="Don't know how to play?" onClick={handleTutorial}/>
+                </div>
+            )}
+            <div className="painter-container">
+                <img className="painter-img" src="/img/painter-girl.png" alt="painter"></img>
             </div>
             <div className="home-menu-container">
                 <div className="button-container">
                     <h1>Ryktet går!</h1>
                 </div>
-                <div className="button-container">
-                    <Button size="large" text="New game" onClick={handleNewGame}/>
-                </div>
+                {!tutorial && (
+                    <div className="button-container">
+                        <Button size="large" text="New game" onClick={handleNewGame}/>
+                    </div>
+                )}
+                {tutorial && (
+                    <div className="button-container">
+                        <button className="begin-button-large show" onClick={handleNewGame}>Begin</button>
+                    </div>
+                )}
                 <div className="button-container">
                     <Button size="large" text="Join game" onClick={handleJoinGame}/>
                 </div>
                 <div className="button-container">
                     <Button size="large" text="Language" onClick={handleLanguage}/>
                 </div>
+            </div>
+            <div className="painter-container">
+                <img className="painter-img" src="/img/painter-boy.png" alt="painter"></img>
             </div>
         </div>
     );
