9951fd7a6fb9ddcd74248d444424ab6aed799ab4
SimonPislar <51m0np15l3r@gmail.com>
2024-06-07 08:46:19 +0200

    Add images and tutorial support

--- client/src/WordInput.js
+++ client/src/WordInput.js
@@ -1,5 +1,5 @@
 
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import './CSS/WordInput.css';
 import Input from "./Input";
 import Button from "./Button";
@@ -10,7 +10,13 @@ function WordInput() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const email = queryParams.get('email');
+    const tutorialString = queryParams.get('tutorial');
+    const tutorial = tutorialString === 'true';
+    const typingIntervalRef = useRef(null);
+    const inputRef = useRef(null);
     const [word, setWord] = useState('');
+    const [isFocused, setIsFocused] = useState(false);
+
 
     const navigate = useNavigate();
 
@@ -35,6 +41,10 @@ function WordInput() {
             .then((data) => {
                 if (data.success) {
                     console.log(data.message);
+                    if (tutorial) {
+                        navigate(`/waiting-for-server?email=${encodeURIComponent(email)}&tutorial=true`);
+                        return;
+                    }
                     navigate(`/waiting-for-server?email=${encodeURIComponent(email)}`);
                 } else {
                     console.log(data.message);
@@ -43,16 +53,75 @@ function WordInput() {
         );
     }
 
+    useEffect(() => {
+        const startTyping = () => {
+            if (tutorial && word === '') {
+                let charIndex = 0;
+                typingIntervalRef.current = setInterval(() => {
+                    setWord((prev) => {
+                        if (charIndex === 17) {
+                            charIndex = 0;
+                            return "Input your word...".slice(0, charIndex);
+                        }
+                        charIndex++;
+                        return "Input your word...".slice(0, charIndex);
+                    });
+                }, 100);
+            }
+        };
+
+        if (!isFocused && tutorial) {
+            const delayBeforeTyping = setTimeout(startTyping, 500);
+            return () => {
+                clearTimeout(delayBeforeTyping);
+                clearInterval(typingIntervalRef.current);
+            };
+        } else {
+            clearInterval(typingIntervalRef.current);
+        }
+    }, [isFocused, tutorial]);
+
+    const handleInputFocus = () => {
+        setIsFocused(true);
+        setWord('');
+    };
+
+    const handleInputBlur = () => {
+        setIsFocused(false);
+    };
+
     return (
         <div className="page-container">
-            <div>
-                <h1>Word Input</h1>
+            <div className="painter-container">
+                <img className="painter-img" src="/img/painter-girl.png" alt="painter"></img>
             </div>
-            <div className="word-input-container">
-                <Input placeholder="Enter word" onChange={handleWordChange}/>
+            <div className="page">
+                <div>
+                    <h1>Word Input</h1>
+                </div>
+                <div className="word-input-container">
+                    {tutorial && (
+                        <div>
+                            <Input type="text" ref={inputRef} onFocus={handleInputFocus} onBlur={handleInputBlur} value={word} onChange={handleWordChange} placeholder="Word" />
+                        </div>
+                    )}
+                    {!tutorial && (
+                        <div>
+                            <Input type="text" value={word} onChange={handleWordChange} placeholder="Word" />
+                        </div>
+                    )}
+                </div>
+                <div>
+                    {tutorial && (
+                        <button className="begin-button-medium show" onClick={handleSubmit}>Submit</button>
+                    )}
+                    {!tutorial && (
+                        <Button size="medium" text="Submit" onClick={handleSubmit} />
+                    )}
+                </div>
             </div>
-            <div>
-                <Button size="medium" text="Submit" onClick={handleSubmit} />
+            <div className="painter-container">
+                <img className="painter-img" src="/img/painter-boy.png" alt="painter"></img>
             </div>
         </div>
     )
