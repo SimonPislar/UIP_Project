7492c8672c92116a463d2424fa37190180d28b3b
SimonPislar <51m0np15l3r@gmail.com>
2024-06-11 17:26:15 +0200

    Add language support

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './CSS/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignInPage from "./SignInPage";
 import Register from "./Register";
 import Home from "./Home";
@@ -19,32 +19,33 @@ import Canvas from "./Canvas";
 import Guess from "./Guess";
 import DisplayGameResult from "./DisplayGameResult";
 import Tutorial from "./Tutorial";
+import { LanguageProvider } from './LanguageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/sign-in" element={<SignInPage/>} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/new-game" element={<NewGame />} />
-            <Route path="/join-game" element={<JoinGame />} />
-            <Route path="/language" element={<Language />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="/lobby" element={<Lobby />} />
-            <Route path="/input-word" element={<WordInput />} />
-            <Route path="/waiting-for-server" element={<WaitingForServer />} />
-            <Route path="/canvas" element={<Canvas />} />
-            <Route path="/guess" element={<Guess />} />
-            <Route path="display-end-game" element={<DisplayGameResult />} />
-            <Route path="/tutorial" element={<Tutorial />} />
-        </Routes>
-    </BrowserRouter>
+    <LanguageProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/sign-in" element={<SignInPage />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/new-game" element={<NewGame />} />
+                <Route path="/join-game" element={<JoinGame />} />
+                <Route path="/language" element={<Language />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="/lobby" element={<Lobby />} />
+                <Route path="/input-word" element={<WordInput />} />
+                <Route path="/waiting-for-server" element={<WaitingForServer />} />
+                <Route path="/canvas" element={<Canvas />} />
+                <Route path="/guess" element={<Guess />} />
+                <Route path="/display-end-game" element={<DisplayGameResult />} />
+                <Route path="/tutorial" element={<Tutorial />} />
+            </Routes>
+        </BrowserRouter>
+    </LanguageProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
