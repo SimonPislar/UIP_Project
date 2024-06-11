da006a8f0d7f217966fa47d83a9f7f5c3342c0f6
SimonPislar <51m0np15l3r@gmail.com>
2024-06-11 17:26:28 +0200

    Add documentation

--- client/src/Input.js
+++ client/src/Input.js
@@ -2,6 +2,7 @@ import React from "react";
 import './CSS/Input.css';
 
 function Input({ type, placeholder, value, onChange, onFocus, onBlur, ref }) {
+    // This is the JSX code for the Input component used throughout the application
     return (
         <input className="input" ref={ref} onBlur={onBlur} onFocus={onFocus} type={type} value={value} onChange={onChange} placeholder={placeholder}/>
     );
