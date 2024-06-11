7537508299a2cc9f101d2b8c0cc528a64aed02f9
SimonPislar <51m0np15l3r@gmail.com>
2024-06-11 17:23:19 +0200

    Add documentation

--- client/src/Account.js
+++ client/src/Account.js
@@ -3,6 +3,7 @@ import {useLocation} from "react-router-dom";
 
 function Account() {
 
+    // Get the email from the URL
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const email = queryParams.get('email');
