09ea4cb22b17e8997711e027c3b738508d3258f1
SimonPislar <51m0np15l3r@gmail.com>
2024-06-11 17:23:49 +0200

    Add documentation

--- client/src/Button.js
+++ client/src/Button.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './CSS/Button.css';
 
 function Button({ size, text, onClick, disabled, isHighlighted, extra }) {
+    // This is the JSX code for the Button component used throughout the application
     return (
         <button
             className={`button ${size} ${isHighlighted ? 'highlighted' : ''}`}
