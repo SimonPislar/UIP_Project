a41b51002c919ae1231eb0a476f92d0dbc8abd98
SimonPislar <51m0np15l3r@gmail.com>
2024-06-11 17:27:31 +0200

    Add documentation

--- client/src/LinkButton.js
+++ client/src/LinkButton.js
@@ -2,6 +2,7 @@ import React from "react";
 import './CSS/LinkButton.css';
 
 function LinkButton({ onClick, text, size }) {
+    // This is the JSX code for the LinkButton component used throughout the application
     return (
         <div>
             <button className={`sign-up-link ${size}`} onClick={onClick}>{text}</button>
